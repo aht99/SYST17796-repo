92e4fbb80227ab4c31b43faf1b5fb645cfe965d2
Alexander Tran <tran62@sheridancollege.ca>
2021-02-02 10:53:10 -0500

    First commit

---
+++ src/studentlist/Student.java
@@ -0,0 +1,31 @@
+/*
+ * Alexander Tran
+ * Student ID: 991500919
+ * SYST10199 - Web Programming
+ */
+package studentlist;
+
+/**
+ * This class represents students in our application
+ * 
+ * @author AlexH
+ */
+public class Student {
+    
+    private String name;
+    
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+}
---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,30 @@
+package studentlist;
+
+import java.util.Scanner;
+
+public class StudentList {
+
+    public static void main(String[] args) {
+        
+        String[] students = new String[6];
+        
+//        students[0] = "John";
+//        students[1] = "Mary";
+//        students[2] = "May";
+//        students[3] = "Kamal";
+//        students[4] = "Susan";
+//        students[5] = "Nancy";
+        
+        for(int i = 0; i < students.length; i++){
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter the student name: ");
+            students[i] = input.nextLine();
+        }
+        
+        for(int i = 0; i < students.length; i++){
+            System.out.println(students[i]);
+        }
+        
+    }
+    
+}
